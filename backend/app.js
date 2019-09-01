78b686642b23e3b6eaaa92d3e56dd2bf2c52d718
SlashMSU <slashmsu@gmail.com>
2019-09-01 23:54:23 +0200

    refactoring

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,7 @@ mongoose
   });
 
 app.use(bodyParser.json());
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("backend/images")));
 
